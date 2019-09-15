02e6634a59c344fcfca153da38bb88b047bf71a7
Evgeny Samsonov <bitniks@yandex.ru>
2019-09-15 12:26:15 +0300

    ATR индикатор: вынести тестовые данные

--- atr_indicator_test.go
+++ atr_indicator_test.go
@@ -8,43 +8,17 @@ import (
 
 const epsilon = 1e-6
 
-func TestAtrIndicator_Compute(t *testing.T) {
+func TestAtrIndicator_Calculate(t *testing.T) {
 	series := NewTimeSeries()
 
-	candleData := []struct {
-		high  float64
-		low   float64
-		open  float64
-		close float64
-	}{
-		{high: 23, low: 21.27, open: 21.3125, close: 22.1044},
-		{high: 23.31999, low: 22.15, open: 22.15, close: 23.21608},
-		{high: 23.2755, low: 22.111, open: 23.2755, close: 22.20585},
-		{high: 22.58, low: 22.015, open: 22.166, close: 22.2},
-		{high: 22.52, low: 22.13501, open: 22.29969, close: 22.15},
-		{high: 22.3, low: 21.601, open: 22.20443, close: 21.861},
-		{high: 22.1, low: 21.45, open: 21.8499, close: 22.09},
-		{high: 22.38, low: 22.0511, open: 22.111, close: 22.25952},
-		{high: 22.488, low: 21.61, open: 22.3005, close: 21.71516},
-		{high: 22.789, low: 21.61001, open: 21.8, close: 22.59434},
-		{high: 22.699, low: 22.351, open: 22.55, close: 22.64},
-		{high: 22.73994, low: 22.45001, open: 22.595, close: 22.56998},
-		{high: 22.69, low: 21.8, open: 22.605, close: 22.37001},
-		{high: 22.58, low: 22.26, open: 22.493, close: 22.397},
-		{high: 22.442, low: 22.0011, open: 22.397, close: 22.099},
-		{high: 22.16, low: 21.72, open: 22.09, close: 21.937},
-	}
-
-	candleTime := time.Now()
-	for _, item := range candleData {
-		candle := NewCandle(candleTime)
+	for _, item := range GetTestCandles() {
+		candle := NewCandle(time.Unix(item.time, 0))
 		candle.High = item.high
 		candle.Low = item.low
 		candle.Open = item.open
 		candle.Close = item.close
 
 		series.AddCandle(candle)
-		candleTime = candleTime.Add(time.Minute)
 	}
 
 	testCases := []struct {
