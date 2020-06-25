e20b11754156a4293f45efe20f325b5738173c16
Evgeny Samsonov <bitniks@yandex.ru>
2020-06-25 23:06:11 +0300

    VWAP indicator

---
+++ indicator/volume_weighted_average_price.go
@@ -0,0 +1,108 @@
+package indicator
+
+import (
+	"github.com/evsamsonov/trading-timeseries/timeseries"
+	"sync"
+	"time"
+)
+
+const oneDay = time.Hour * 24
+
+// VolumeWeightedAveragePrice represents indicator to calculate volume-weighted average price (VWAP).
+// More details https://en.wikipedia.org/wiki/Volume-weighted_average_price
+type VolumeWeightedAveragePrice struct {
+	series *timeseries.TimeSeries
+	cache  *vwapCache
+}
+
+func NewVolumeWeightedAveragePrice(series *timeseries.TimeSeries) *VolumeWeightedAveragePrice {
+	return &VolumeWeightedAveragePrice{
+		series: series,
+		cache:  newVwapCache(),
+	}
+}
+
+func (v *VolumeWeightedAveragePrice) Calculate(index int) float64 {
+	unit, ok := v.cache.Get(index)
+	if ok {
+		return unit.vwap
+	}
+
+	day := v.series.Candle(index).Time.Truncate(oneDay)
+
+	startIndex, unit := v.findLastCalculated(index, day)
+	volumeTotal := unit.volumeTotal
+	priceVolumeTotal := unit.priceVolumeTotal
+
+	for i := startIndex; ; i++ {
+		candle := v.series.Candle(i)
+		if !candle.Time.Truncate(oneDay).Equal(day) {
+			break
+		}
+
+		typicalPrice := calcTypicalPrice(candle)
+		priceVolumeTotal = priceVolumeTotal + float64(candle.Volume)*typicalPrice
+		volumeTotal = volumeTotal + candle.Volume
+
+		vwap := priceVolumeTotal / float64(volumeTotal)
+
+		v.cache.Add(i, vwapUnit{
+			vwap:             vwap,
+			priceVolumeTotal: priceVolumeTotal,
+			volumeTotal:      volumeTotal,
+		})
+	}
+
+	unit, _ = v.cache.Get(index)
+	return unit.vwap
+}
+
+func (v *VolumeWeightedAveragePrice) findLastCalculated(index int, day time.Time) (startIndex int, item vwapUnit) {
+	for i := index - 1; i >= 0; i-- {
+		candle := v.series.Candle(i)
+		if !candle.Time.Truncate(oneDay).Equal(day) {
+			return i + 1, vwapUnit{}
+		}
+
+		item, ok := v.cache.Get(i)
+		if ok {
+			return i + 1, item
+		}
+	}
+
+	return 0, vwapUnit{}
+}
+
+func calcTypicalPrice(candle *timeseries.Candle) float64 {
+	return (candle.High + candle.Low + candle.Close) / 3
+}
+
+type vwapCache struct {
+	mu    sync.RWMutex
+	items map[int]vwapUnit
+}
+
+func newVwapCache() *vwapCache {
+	return &vwapCache{
+		items: make(map[int]vwapUnit),
+	}
+}
+
+type vwapUnit struct {
+	vwap             float64
+	priceVolumeTotal float64
+	volumeTotal      int64
+}
+
+func (v *vwapCache) Get(index int) (vwapUnit, bool) {
+	v.mu.RLock()
+	defer v.mu.RUnlock()
+	val, ok := v.items[index]
+	return val, ok
+}
+
+func (v *vwapCache) Add(index int, item vwapUnit) {
+	v.mu.Lock()
+	defer v.mu.Unlock()
+	v.items[index] = item
+}
---
+++ indicator/volume_weighted_average_price_test.go
@@ -0,0 +1,55 @@
+package indicator
+
+import (
+	"fmt"
+	"github.com/evsamsonov/trading-timeseries/timeseries"
+	"github.com/stretchr/testify/assert"
+	"log"
+	"testing"
+	"time"
+)
+
+func TestVolumeWeightedAveragePrice_Calculated(t *testing.T) {
+	series := timeseries.New()
+
+	assert.Nil(t, series.AddCandle(createCandle("2020-06-24T00:00:00+00:00", 1, 2, 3, 100)))
+	assert.Nil(t, series.AddCandle(createCandle("2020-06-25T00:00:00+00:00", 4, 5, 6, 200)))
+	assert.Nil(t, series.AddCandle(createCandle("2020-06-25T01:00:00+00:00", 7, 8, 9, 300)))
+	assert.Nil(t, series.AddCandle(createCandle("2020-06-26T00:00:00+00:00", 10, 11, 12, 400)))
+
+	tests := []struct {
+		index int
+		want  float64
+	}{
+		{index: 0, want: 2},
+		{index: 1, want: 5},
+		{index: 2, want: 6.8},
+	}
+
+	vwapIndicator := NewVolumeWeightedAveragePrice(series)
+
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("index=%d", tt.index), func(t *testing.T) {
+			vwap := vwapIndicator.Calculate(tt.index)
+			assert.InEpsilon(t, tt.want, vwap, float64EqualityThreshold)
+		})
+	}
+
+}
+
+func createCandle(date string, high, low, close float64, volume int64) *timeseries.Candle {
+	candle := timeseries.NewCandle(parseDate(date))
+	candle.High = high
+	candle.Low = low
+	candle.Close = close
+	candle.Volume = volume
+	return candle
+}
+
+func parseDate(t string) time.Time {
+	date, err := time.Parse(time.RFC3339, t)
+	if err != nil {
+		log.Fatal("failed to parse time", err)
+	}
+	return date
+}
