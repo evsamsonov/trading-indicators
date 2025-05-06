8ae959d2062cac71f1cf534d80617e1181ebe2ef
Evgeny Samsonov <bitniks@yandex.ru>
2025-05-07 00:06:24 +0300

    Average volume: Add filter

--- indicator/average_volume.go
+++ indicator/average_volume.go
@@ -1,36 +1,64 @@
 package indicator
 
-import "github.com/evsamsonov/trading-timeseries/timeseries"
+import (
+	"sync"
 
-// AverageVolume is a indicator to calculate average volume
-// for given smoothInterval using simple moving average
+	"github.com/evsamsonov/trading-timeseries/timeseries"
+)
+
+type AverageVolumeFilterFunc func(candle *timeseries.Candle) bool
+
+// AverageVolume calculates a simple moving average of volume
+// over a given period, optionally filtering candles.
 type AverageVolume struct {
 	series *timeseries.TimeSeries
 	period int
+	mu     sync.RWMutex
 	cache  map[int]float64
+	filter AverageVolumeFilterFunc
 }
 
-func NewAverageVolume(series *timeseries.TimeSeries, period int) *AverageVolume {
+func NewAverageVolume(series *timeseries.TimeSeries, period int, filter AverageVolumeFilterFunc) *AverageVolume {
 	return &AverageVolume{
 		series: series,
 		period: period,
 		cache:  make(map[int]float64),
+		filter: filter,
 	}
 }
 
-func (av *AverageVolume) Calculate(index int) float64 {
-	if index < av.period-1 {
+func (a *AverageVolume) Calculate(index int) float64 {
+	if index < a.period-1 {
 		return 0
 	}
 
-	if val, ok := av.cache[index]; ok {
+	a.mu.RLock()
+	val, ok := a.cache[index]
+	a.mu.RUnlock()
+	if ok {
 		return val
 	}
 
 	volumeSum := 0.0
-	for i := index - av.period + 1; i <= index; i++ {
-		volumeSum += float64(av.series.Candle(i).Volume)
+	count := 0
+	for i := index; count < a.period; i-- {
+		if i < 0 {
+			return 0
+		}
+
+		candle := a.series.Candle(i)
+		if a.filter != nil && !a.filter(candle) {
+			continue
+		}
+		volumeSum += float64(candle.Volume)
+		count++
 	}
 
-	return volumeSum / float64(av.period)
+	avg := volumeSum / float64(a.period)
+
+	a.mu.Lock()
+	a.cache[index] = avg
+	a.mu.Unlock()
+
+	return avg
 }
