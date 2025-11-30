d2d5ced5dc72de4bd7d7e150ce65e64b28665b03
Evgeny Samsonov <bitniks@yandex.ru>
2025-11-30 23:26:45 +0300

    Alias for AverageVolumeFilterFunc

--- indicator/filter.go
+++ indicator/filter.go
@@ -3,3 +3,6 @@ package indicator
 import "github.com/evsamsonov/trading-timeseries/timeseries"
 
 type FilterFunc func(i int, candle *timeseries.Candle) bool
+
+// AverageVolumeFilterFunc is deprecated, use FilterFunc instead.
+type AverageVolumeFilterFunc = FilterFunc
